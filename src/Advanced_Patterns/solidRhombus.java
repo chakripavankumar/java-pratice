8ba8f5dc3a833d979a2ba1df23d1f71f364ceab6
pavan kumar <siriyalapavankumar@gmail.com>
2024-02-01 19:22:32 +0530

    "tail"

--- src/Advanced_Patterns/solidRhombus.java
+++ src/Advanced_Patterns/solidRhombus.java
@@ -10,7 +10,7 @@ public class solidRhombus {
             for (int j = 1; j <=5 ; j++) {
                 System.out.print("*");
             }
-            System.out.println();
+            System.out.println("*");
         }
     }
 }
