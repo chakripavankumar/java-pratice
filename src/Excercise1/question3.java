9e9f9b5c6c40c8853cf450258979cfabb3eec46e
pavan kumar <siriyalapavankumar@gmail.com>
2024-02-02 20:20:41 +0530

    "pratice-4/10✅"

--- src/Excercise1/question3.java
+++ src/Excercise1/question3.java
@@ -10,7 +10,7 @@ public class question3 {
         int n1= sc.nextInt();
         System.out.println("please enter the second number");
         int n2=sc.nextInt();
-      sumoftwo(n1);
+      sumoftwo(n1,n2);
     }
     public  static  int sumoftwo( int n1, int n2){
         return ( n1>n2) ? n1:n2;
